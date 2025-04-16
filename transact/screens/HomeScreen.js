232a105c6e84ae8f0f96ac45a23ca5e531c57ac7
Dx_craig <steviedoe20@gmail.com>
2025-04-16 07:19:14 +0000

    Enhance HomeScreen and Settings components with layout and styling improvements

--- transact/screens/HomeScreen.js
+++ transact/screens/HomeScreen.js
@@ -1,14 +1,62 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
 
-const HomeScreen = () => {
+
+
+export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+        <View style = {styles.group}>
+            <View style = {styles.profileIcon}>
+                <Image source={require('../assets/profile.png')}/>
+            </View>
+
+
+            <View>
+                <Text>Welcome back,</Text>
+                <Text>Eric Atsu</Text>
+            </View>
+
+            <View style = {styles.searchIcon}>
+                <Image source={require('../assets/search.png')}/>
+            </View>
+        </View>
+
+        <View style = {styles.card}>
+            <Image source={require('../assets/Card.png')}/>
+        </View>
+
+        
     </View>
   )
 }
 
-export default HomeScreen
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: 'flex',
+  },
+    group: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingTop: 50, 
+       
+        
+    },
+    profileIcon: {
+        borderRadius: 50,
+        paddingLeft: 20,
+        paddingRight: 20,
+    },
+    searchIcon: {
+        borderRadius: 50,
+        paddingLeft: 20,
+        paddingRight: 20,
+        marginLeft: 'auto',
+    },
+    card: {
+        marginTop: 30,
+        borderRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+})
--- transact/screens/Settings.js
+++ transact/screens/Settings.js
@@ -1,14 +1,106 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, useColorScheme , Appearance} from 'react-native'
+import React , {useState} from 'react'
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+
+const data = [
+  { id: '1', title: 'Language' },
+  { id: '2', title: 'My Profile' },
+  { id: '3', title: 'Contact Us' },
+  { id: '4', title: 'Change Password' },
+  { id: '5', title: 'Privacy Policy' },
+]
+
+const Item = ({ title, onPress }) => (
+  <TouchableOpacity onPress={onPress}>
+    <View style={styles.group}>
+      <Text style={styles.title}>{title}</Text>
+      <AntDesign name="right" size={24} color="black" />
+    </View>
+  </TouchableOpacity>
+)
 
 const Settings = () => {
+
   return (
-    <View>
-      <Text>Settings</Text>
+    <View style={[styles.container]}>
+      <View style={styles.subtitle}>
+        <Text style={[styles.contain]}>Settings</Text>
+      </View>
+
+      <View>
+        <FlatList
+          data={data}
+          renderItem={({ item }) =>
+            <View >
+              <Item title={item.title} />
+            </View>
+          }
+          keyExtractor={item => item.id}
+        />
+      </View>
+      <View style={styles.switchContainer}>
+        
+      </View>
     </View>
   )
 }
 
 export default Settings
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    paddingVertical: 20,
+    marginVertical: 8,
+    fontWeight: 'bold',
+  },
+  settingsGroup: {
+    marginTop: 60,
+    marginHorizontal: 20,
+  },
+  subtitle: {
+    marginTop: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contain: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  group: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderBottomColor: 'grey',
+    borderBottomWidth: 1,
+    marginHorizontal: 20,
+    paddingLeft: 10,
+    borderRadius: 10,
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    marginTop: 20,
+
+  },
+  lightContainer: {
+    backgroundColor: 'white',
+  },
+  darkContainer: {
+    backgroundColor: '#242c40',
+  },
+  lightThemeText: {
+    color: '#242c40',
+  },
+  darkThemeText: {
+    color: '#d0d0c0',
+  },
+})
